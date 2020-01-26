02004f43bfe941f3af37b8cbb869bdae686f023b
GGKL <60282294+qq92466808@users.noreply.github.com>
2020-01-26 12:20:40 +0800

    Add files via upload

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,26 @@
+﻿
+#include <Windows.h>
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	HDC hdc = GetWindowDC(GetDesktopWindow());
+
+	SetTextColor(hdc, RGB(255, 170, 0));
+	SetBkMode(hdc, TRANSPARENT);
+	int x, y;
+	while (1)
+	{
+		POINT p;
+		GetCursorPos(&p);
+		x = p.x, y = p.y;
+		TextOut(hdc, x, y, "123456789", 9);
+		TextOut(hdc, x, y, "         ", 9);
+		--x;
+		Sleep(100);
+
+	}
+}
